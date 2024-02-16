e720162db8b0bbdfdb7c5739c66e16e2a4d423d8
Houssam Elbadissi <skaldebane@gmail.com>
2024-02-16 18:19:21 +0100

    Updated demo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,33 @@
 <script lang="ts">
     import { cleanable } from "$lib/index.js";
 
-    const parent = cleanable(0);
+    let logs: string[] = [];
+    const state = cleanable(0);
 
-    parent.subscribe((value) => {
-        console.log("parent changed", value);
-        return () => console.log(`cleaning up old parent ${value}...`);
+    state.subscribe((value) => {
+        log(`state = ${value}`);
+        return () => log(`cleaning up ${value}...`);
     });
+
+    function log(msg: string) {
+        console.log(msg);
+        logs = [msg, ...logs];
+    }
+
+    function increment() {
+        logs = ["$state++", "", ...logs];
+        $state++;
+    }
 </script>
 
-<h1>Parent: {$parent}</h1>
-<button on:click={() => $parent++}>Increment</button>
+<h1>state = {$state}</h1>
+<button on:click={increment}>Increment</button>
+{#each logs as log}
+    <p class={log === "$state++" ? "highlighted" : ""}>{log}</p>
+{/each}
+
+<style>
+    .highlighted {
+        color: green;
+    }
+</style>
